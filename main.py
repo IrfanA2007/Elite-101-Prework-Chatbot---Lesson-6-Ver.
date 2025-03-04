58fd4fe98b6b7497e2da63d73e4ba4a54fb6ca61
IrfanA2007 <ia.irfan2007@gmail.com>
2025-03-04 00:16:53 +0000

    finished

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 
 def main_menu():
-    print("~~~~~~~~~~~~~~~\n1. Placeholder\n2. Placeholder\n3.Placeholder\n4. Exit Chatbot\n~~~~~~~~~~~~~~~")
+    order_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print("~~~~~~~~~~~~~~~\n1. Return\n2. Exchange\n3. Exit\n~~~~~~~~~~~~~~~")
     option = int(input("Please input what you would like to do: "))
     if option == 1:
-        print("Doing something cool...1")
+        if(input("Input your order number") in order_list):
+            print("Your request has been processed.")
+        else:
+            print("Not a valid order number")
     elif option == 2:
         print("Doing something cool...2")
     elif option == 3:
-        print("Doing something cool...3")
-    if option == 4:
         return False
-    else:
-        return True
 
 
 loop = True
 
-print("Hello, welcome to the chatbot interface!")
-
-name = input("Please input your name: ")
-age = input(f"Hello {name}, please input your age: ")
+print("Hello, welcome to customer service")
 
 
 while loop:
     loop = main_menu()
 
-print("Thank you for using the chatbot!")
+print("Have a nice day!")
 
 
 
